ea76f7707d80b79553b67e2e042222309f7b3af8
Daniel Meilleur <daniel.meilleur@gmail.com>
2018-02-07 16:23:32 +0800

    process.env.CWD pour heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict'
 // dependencies
+process.env.CWD = process.cwd()
 const express = require ('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -10,8 +11,10 @@ const upload = multer({dest: 'uploads/'})
 
 app.use(bodyParser.json())
 app.use(cors())
+app.use(express.static(process.env.CWD + '/public'))
 
 app.post('/upload', upload.single('file'), (req, res, next) =>  {
+    return res.json(req.file.size)
 })
 
 const PORT = process.env.PORT || 3000;
